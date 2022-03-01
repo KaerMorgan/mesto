d00e2a4dafaf419c3e5f156b37367e64df399303
Danil Filatov <Subdante@gmail.com>
2022-03-01 17:30:04 +0300

    added mobile adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,13 @@ let popup = document.querySelector('.popup');
 let popupOpen = document.querySelector('.profile-info__edit-button');
 let popupClose = document.querySelector('.popup__close')
 
+let name = document.querySelector('.profile-info__name');
+let occupation = document.querySelector('.profile-info__occupation');
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__name');
+let occupationInput = formElement.querySelector('.popup__occupation');
+
 // открытие по кнопке
 popupOpen.addEventListener('click', function() {
   popup.classList.add('popup__opened');
@@ -10,6 +17,10 @@ popupOpen.addEventListener('click', function() {
 // закрытие по кнопке
 popupClose.addEventListener('click', function() {
   popup.classList.remove('popup__opened');
+
+  // стираем введённые символы при закрытии
+  nameInput.value = name.textContent;
+  occupationInput.value = occupation.textContent;
 });
 
 // хитрый трюк которому я научился на проекте :)
@@ -19,13 +30,6 @@ document.addEventListener('click', (e) => {
   }
 })
 
-let name = document.querySelector('.profile-info__name');
-let occupation = document.querySelector('.profile-info__occupation');
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let occupationInput = formElement.querySelector('.popup__occupation');
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
@@ -34,3 +38,13 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+// через айди, потому что пытался заставить работать по крайней мере одну
+let like = document.querySelector('#element__like')
+like.addEventListener("mouseover", function() {
+  like.style.fill = 'black';
+});
+
+like.addEventListener("mouseout", function() {
+  like.style.fill = 'none';
+});

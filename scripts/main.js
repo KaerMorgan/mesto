469117b6b99bc8ceeb15e5b631a83f307e1796ea
Danil Filatov <Subdante@gmail.com>
2022-03-02 15:04:29 +0300

    fixed height popup and fonts

--- scripts/main.js
+++ scripts/main.js
@@ -11,12 +11,12 @@ let occupationInput = formElement.querySelector('.popup__occupation');
 
 // открытие по кнопке
 popupOpen.addEventListener('click', function() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
 });
 
 // закрытие по кнопке
 popupClose.addEventListener('click', function() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 
   // стираем введённые символы при закрытии
   nameInput.value = name.textContent;
@@ -26,31 +26,24 @@ popupClose.addEventListener('click', function() {
 // хитрый трюк которому я научился на стороннем проекте :)
 document.addEventListener('click', (e) => {
   if (e.target === popup) {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
   }
-})
+});
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
   name.textContent = nameInput.value;
   occupation.textContent = occupationInput.value;
-}
+};
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-//
-//
-//
-
 
 
 let elements = document.querySelectorAll('.element__like');
 
-
 elements.forEach(function(item) {
   item.addEventListener('click', function() {
-    console.log('ну кликнул и что');
     item.innerHTML = `<img src="./images/liked.png" alt="Мне нравится">`
-  })
+  });
 });

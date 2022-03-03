bab0358700b389a1eb192e75b21ad298500a3d80
Danil Filatov <Subdante@gmail.com>
2022-03-03 16:38:31 +0300

    revue fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,60 @@
 let popup = document.querySelector('.popup');
-let popupOpen = document.querySelector('.profile-info__edit-button');
-let popupClose = document.querySelector('.popup__close')
+let editButton = document.querySelector('.profile-info__edit-button');
+let closeButton = document.querySelector('.popup__close')
 
 let name = document.querySelector('.profile-info__name');
 let occupation = document.querySelector('.profile-info__occupation');
 
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let occupationInput = formElement.querySelector('.popup__occupation');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('#popup__input-name');
+let occupationInput = formElement.querySelector('#popup__input-occupation');
 
 // открытие по кнопке
-popupOpen.addEventListener('click', function() {
+function popupOpen() {
   popup.classList.add('popup_opened');
-});
+
+  nameInput.value = name.textContent
+  occupationInput.value = occupation.textContent
+}
+editButton.addEventListener('click', popupOpen);
 
 // закрытие по кнопке
-popupClose.addEventListener('click', function() {
+function popupClose() {
   popup.classList.remove('popup_opened');
 
   // стираем введённые символы при закрытии
   nameInput.value = name.textContent;
   occupationInput.value = occupation.textContent;
-});
+}
+closeButton.addEventListener('click', popupClose);
 
-// хитрый трюк которому я научился на стороннем проекте :)
-document.addEventListener('click', (e) => {
-  if (e.target === popup) {
-    popup.classList.remove('popup_opened');
-  }
-});
+// закрытие попапа по клику на обложку игнорирующее всплытие до формы (не относится к заданию)
+// document.addEventListener('click', (e) => {
+//   if (e.target === popup) {
+//     popup.classList.remove('popup_opened');
+//   }
+// });
 
+
+// сохранение введённых данных
 function formSubmitHandler(evt) {
+  // отменяем перезагрузку страницы
   evt.preventDefault();
 
+  // перезаписываем данные из инпутов при нажатии
   name.textContent = nameInput.value;
   occupation.textContent = occupationInput.value;
-};
 
+  popupClose()
+};
 formElement.addEventListener('submit', formSubmitHandler);
 
 
+// изменение состояния картинки лайка при нажатии
 let elements = document.querySelectorAll('.element__like');
 
 elements.forEach(function(item) {
   item.addEventListener('click', function() {
-    item.innerHTML = `<img src="./images/liked.png" alt="Мне нравится">`
+    item.classList.toggle('element__like_pressed');
   });
 });
